7a202bce23c1b2c23aa8183ffc93c90ffe12cf5d
bintangpananjung <pananjungbintang@gmail.com>
2023-03-17 22:09:57 +0700

    add endpoint duration, timer, fix getdata, tidy up

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 var express = require("express");
 var app = express();
 var route = require("./routes");
-const mqtt = require("mqtt");
 const { mongoconnect, mongoclient } = require("./mongodb");
-// mongoconnect().catch(console.error);
+const { client } = require("./mqtt");
+const cors = require("cors");
+
 mongoclient.connect().catch(console.error);
 
-const host = "broker.emqx.io";
-const port = "1883";
-const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
-const connectUrl = `mqtt://${host}:${port}`;
-const client = mqtt.connect(connectUrl, {
-  clientId,
-  clean: true,
-  connectTimeout: 4000,
-  reconnectPeriod: 1000,
-});
 client.on("connect", function () {
   client.subscribe("durationOn", function (err) {
     if (err) {
       console.log(err.message);
     }
   });
-  client.subscribe("ledStatus", function (err) {
+  client.subscribe("dataStatus", function (err) {
     if (err) {
       console.log(err.message);
     }
@@ -31,37 +22,37 @@ client.on("connect", function () {
 
 client.on("message", async function (topic, message) {
   // message is Buffer
-  console.log(message.toString());
+  // console.log(topic);
   if (topic === "durationOn") {
+    const timestamp = new Date(Date.now());
+    const tempMessage = message.toString().split(":");
     await mongoclient
       .db("mqttdb")
-      .collection("durationOn")
+      .collection("duration")
       .insertOne({
-        onDuration: parseInt(message.toString()),
-        timestamp: new Date(Date.now()),
+        onDuration: parseInt(tempMessage[0]),
+        offDuration: parseInt(tempMessage[1]),
+        timestamp: timestamp,
       });
   }
-  if (topic === "ledStatus") {
+  if (topic === "dataStatus") {
+    const tempData = message.toString().split(":");
+    // console.log(tempData);
+
     await mongoclient
       .db("mqttdb")
-      .collection("status")
+      .collection("dataStatus")
       .insertOne({
-        status: parseInt(message.toString()),
+        status: parseInt(tempData[0]),
+        timerStatus: tempData[1],
+        duration: tempData[2],
         timestamp: new Date(Date.now()),
       });
   }
   // client.end();
 });
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-route.post("/status", function (req, res) {
-  try {
-    // console.log(req.body);
-    client.publish("requestStatus", req.body.control);
-  } catch (error) {
-    console.log(error.message);
-  }
-  res.send("POST route on things.");
-});
 
 app.use("/", route);
 app.listen(8000, function () {
--- routes.js
+++ routes.js
@@ -1,23 +1,131 @@
 var express = require("express");
 const { mongoclient } = require("./mongodb");
-const durationcollection = mongoclient.db("mqttdb").collection("durationOn");
-const statuscollection = mongoclient.db("mqttdb").collection("status");
+const { client } = require("./mqtt");
+const durationcollection = mongoclient.db("mqttdb").collection("duration");
+const dataStatusCollection = mongoclient.db("mqttdb").collection("dataStatus");
+const { delay } = require("./lib/functions");
 var router = express.Router();
 
-router.get("/durationOn", async function (req, res) {
+router.get("/duration", async function (req, res) {
   await mongoclient.connect();
-  const result = await durationcollection.find().toArray();
+  let result = "no data";
+  if (!req.query.filter) {
+    result = await durationcollection.find().toArray();
+  } else {
+    const data = {
+      onDuration: { $sum: "$onDuration" },
+      offDuration: { $sum: "$offDuration" },
+    };
+    const project = {
+      year: { $year: "$timestamp" },
+      month: { $month: "$timestamp" },
+      day: { $dayOfMonth: "$timestamp" },
+      hour: { $hour: "$timestamp" },
+      // min: { $minute: "$timestamp" },
+      // s: { $second: "$timestamp" },
+    };
+    if (req.query.filter === "second") {
+      result = await durationcollection
+        .aggregate([
+          {
+            $group: {
+              _id: {
+                ...project,
+                minute: { $minute: "$timestamp" },
+                second: { $second: "$timestamp" },
+              },
+              ...data,
+            },
+          },
+        ])
+        .toArray();
+    }
+    if (req.query.filter === "minute") {
+      result = await durationcollection
+        .aggregate([
+          {
+            $group: {
+              _id: {
+                ...project,
+                minute: { $minute: "$timestamp" },
+              },
+              ...data,
+            },
+          },
+        ])
+        .toArray();
+    }
+    if (req.query.filter === "hour") {
+      result = await durationcollection
+        .aggregate([
+          {
+            $group: {
+              _id: {
+                ...project,
+              },
+              ...data,
+              // timestamp: $h,
+            },
+          },
+        ])
+        .toArray();
+    }
+    if (req.query.filter === "day") {
+      result = await durationcollection
+        .aggregate([
+          {
+            $group: {
+              _id: {
+                $dateToString: { format: "%Y-%m-%d", date: "$timestamp" },
+              },
+              ...data,
+            },
+          },
+        ])
+        .toArray();
+    }
+  }
   res.send(result);
 });
-router.get("/status", async function (req, res) {
+router.get("/dataStatus", async function (req, res) {
+  client.publish("requestData", "get");
+  await delay(2000);
   await mongoclient.connect();
-  const result = await statuscollection
+  const result = await dataStatusCollection
     .find()
     .sort({ timestamp: -1 })
     .limit(1)
     .toArray();
   res.send(result[0]);
 });
+router.post("/status", function (req, res) {
+  try {
+    // console.log(req.body);
+    client.publish("control", req.body.control);
+    res.send("Successfully change led status to " + req.body.control);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+router.post("/timer", async function (req, res) {
+  try {
+    // console.log(req.body);
+    client.publish("timer", req.body.timer);
+    await delay(2000);
+    res.send("Successfully change timer to " + req.body.timer);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
+router.post("/duration", async function (req, res) {
+  try {
+    client.publish("duration", req.body.duration);
+    // await delay(2000);
+    res.send("Successfully change duration to " + req.body.duration);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 //export this router to use in our index.js
 module.exports = router;
